64f8bb561d240ace687fcab41c57a1875432b825
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-03-13 14:25:14 +0100

    add express server to handle routing

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const nextJs = require('next')
+const routes = require('./src/routes')
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+const server = express();
+const app = nextJs({ dev, dir: './src' });
+const appHandler = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+
+  // Web app routes
+  server.get('*', appHandler);
+  // Listen on port
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.info(`> Ready http://localhost:${port} <`);
+  });
+}).catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+});
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = () => {
+  return (
+    <div>
+      hello world
+    </div>
+  );
+};
+
+export default home;
