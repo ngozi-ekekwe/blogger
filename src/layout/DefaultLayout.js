bfbfbc9b5926089d28365965591f49326c7d0e4a
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-03-15 19:15:10 +0100

    fix footer

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 const DefaultLayout = ({children}) => {
   return (
